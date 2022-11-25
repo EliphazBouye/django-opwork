eaf0615a03abe68b7349e99dd3d697944867857f
Sophonie Bouye <bouyesophonie@gmail.com>
2022-11-25 08:28:27 +0000

    create first app freelancer

--- upwork_clone/settings.py
+++ upwork_clone/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'upwork_clone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'mariadb_info.cnf',
     }
 }
 
