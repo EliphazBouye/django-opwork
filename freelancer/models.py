5e6edeb5ae09f130545b925c0efe8f98a1b15d9a
Sophonie Bouye <bouyesophonie@gmail.com>
2022-11-28 12:21:03 +0000

    freelancer model and migration

--- freelancer/models.py
+++ freelancer/models.py
@@ -1,35 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
-class Freelance(AbstractUser):
-    username = models.CharField(
-        verbose_name='unique username',
-        max_length=255,
-        unique=True
-    )
-    first_name = models.CharField(
-        verbose_name='first name',
-        max_length=255,
-        unique=True
-    )
-    last_name = models.CharField(
-        verbose_name='last name',
-        max_length=255,
-        unique=True
-    )
-    email = models.EmailField(
-        verbose_name='email address',
-        max_length=255,
-        unique=True,
-    )
-    address = models.CharField(
-        verbose_name='location address',
-        max_length=255,
-    )
+class Freelance(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField()
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'email']
-    
+
     def __str__(self):
         return self.username
-    
