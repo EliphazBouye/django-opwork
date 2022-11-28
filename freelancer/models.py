87aad841479242031556e3e576d40695c8f1d5be
Sophonie Bouye <bouyesophonie@gmail.com>
2022-11-28 11:14:34 +0000

    add git ignore

--- freelancer/models.py
+++ freelancer/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Freelance(AbstractUser):
+    username = models.CharField(
+        verbose_name='unique username',
+        max_length=255,
+        unique=True
+    )
+    first_name = models.CharField(
+        verbose_name='first name',
+        max_length=255,
+        unique=True
+    )
+    last_name = models.CharField(
+        verbose_name='last name',
+        max_length=255,
+        unique=True
+    )
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    address = models.CharField(
+        verbose_name='location address',
+        max_length=255,
+    )
+    bio = models.TextField()
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'email']
+    
+    def __str__(self):
+        return self.username
+    
