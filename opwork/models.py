f31e5fd8b3938ace9c72f3feccb4b739b73a81d0
Eliphaz Bouye <eliphazbouye@gmail.com>
2023-03-16 18:12:32 +0000

    reorganisation of models

--- opwork/models.py
+++ opwork/models.py
@@ -1,9 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, User
+
+class User(AbstractUser):
+    is_freelance = models.BooleanField(default=False)
+    is_client = models.BooleanField(default=False)
+    bio = models.TextField()
+    # profile_picture = models.ImageField(upload_to='profile_pictures')
 
 class Freelance(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name="user")
 
     def __str__(self):
         return self.username
+    
+class Cv(models.Model):
+    freelance = models.OneToOneField(Freelance, on_delete=models.CASCADE)
+    cv = models.FileField(upload_to='cvs')
+    
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return self.firstname
+ 
