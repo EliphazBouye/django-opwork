9571dec890dd6fbfcee6e278c523466250ead885
Eliphaz Bouye <eliphazbouye@gmail.com>
2023-03-16 16:52:22 +0000

    update urls of project

--- opwork_project/urls.py
+++ opwork_project/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('/', include('opwork.urls')),
+    path('', include('opwork.urls')),
     path('admin/', admin.site.urls),
 ]
