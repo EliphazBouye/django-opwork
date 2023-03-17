c372ff83c1f8b66bd5e5e1534f6d7ba9e8206698
Eliphaz Bouye <eliphazbouye@gmail.com>
2023-03-17 08:44:22 +0000

    update models

--- opwork/models.py
+++ opwork/models.py
@@ -5,17 +5,12 @@ class User(AbstractUser):
     is_freelance = models.BooleanField(default=False)
     is_client = models.BooleanField(default=False)
     bio = models.TextField()
-    # profile_picture = models.ImageField(upload_to='profile_pictures')
 
 class Freelance(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name="user")
 
     def __str__(self):
         return self.username
-    
-class Cv(models.Model):
-    freelance = models.OneToOneField(Freelance, on_delete=models.CASCADE)
-    cv = models.FileField(upload_to='cvs')
     
 class Client(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
