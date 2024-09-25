09c67bcb38d26da11d54e4169601f958e3af14d5
Eliphaz Bouye <eliphazbouye@gmail.com>
2024-09-25 23:45:16 +0000

    core(BEAKING CHANGE): big update change project
    change project name, update package, switch from mysql to postgresql

--- gigconnect/wsgi.py
+++ gigconnect/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'opwork_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gigconnect.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'opwork_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gigconnect.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
